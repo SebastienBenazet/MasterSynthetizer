9163cd12572ca271590080ae233087e6772b3977
Sebastien Benazet <s.benazet.59@gmail.com>
2016-10-28 18:06:48 +0200

    Added home picture Changed navigation drawer color

--- app/src/main/java/com/kitbass/mastersynthetizer/MainActivity.java
+++ app/src/main/java/com/kitbass/mastersynthetizer/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -62,6 +63,7 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(getApplicationContext(), "Coming soon !", Toast.LENGTH_SHORT).show();
             return true;
         }
 
