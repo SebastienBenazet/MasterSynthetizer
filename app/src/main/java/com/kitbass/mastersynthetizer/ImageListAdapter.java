b24c9abb6a7f82cbbf796aa7ec3448ff47015f30
Sebastien Benazet <s.benazet.59@gmail.com>
2017-03-23 20:17:10 +0100

    -Fixed stretched images. -Fixed clic event on images

--- app/src/main/java/com/kitbass/mastersynthetizer/ImageListAdapter.java
+++ app/src/main/java/com/kitbass/mastersynthetizer/ImageListAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -14,29 +15,41 @@ public class ImageListAdapter extends ArrayAdapter {
     private Context context;
     private LayoutInflater inflater;
 
-    private Integer[] imageRessources;
+    private Integer[] imageResources;
 
-    public ImageListAdapter(Context context, Integer[] imageUrls) {
-        super(context, R.layout.listview_item_image, imageUrls);
+    static class ViewHolder {
+        ImageView mImageView;
+    }
+
+    public ImageListAdapter(Context context, Integer[] image) {
+        super(context, R.layout.listview_item_image, image);
 
         this.context = context;
-        this.imageRessources = imageUrls;
+        this.imageResources = image;
 
         inflater = LayoutInflater.from(context);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
+        // ViewHolder pattern in play
+        ViewHolder viewHolder;
         if (null == convertView) {
+            viewHolder = new ViewHolder();
             convertView = inflater.inflate(R.layout.listview_item_image, parent, false);
+            viewHolder.mImageView = (ImageView) convertView.findViewById(R.id.iv_button);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
         Picasso
                 .with(context)
-                .load(imageRessources[position])
+                .load(imageResources[position])
                 .fit()
                 .centerInside()
-                .into((ImageView) convertView);
+                .into(viewHolder.mImageView);
 
         return convertView;
     }
--- app/src/main/java/com/kitbass/mastersynthetizer/PicassoActivity.java
+++ app/src/main/java/com/kitbass/mastersynthetizer/PicassoActivity.java
@@ -1,8 +1,13 @@
 package com.kitbass.mastersynthetizer;
 
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
@@ -21,6 +26,10 @@ public class PicassoActivity extends AppCompatActivity {
 
     public static Integer[] imgResourcesHeenok;
     private AdView mAdView;
+    private boolean loaded;
+    private SoundPool soundPool;
+
+    private static final int MAX_STREAMS = 7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +42,19 @@ public class PicassoActivity extends AppCompatActivity {
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
 
+        if (Build.VERSION.SDK_INT >= 21) {
+            AudioAttributes audioAttrib = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_GAME)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+
+            SoundPool.Builder builder = new SoundPool.Builder();
+            builder.setAudioAttributes(audioAttrib).setMaxStreams(MAX_STREAMS);
+            this.soundPool = builder.build();
+        } else {
+            this.soundPool = new SoundPool(MAX_STREAMS, AudioManager.STREAM_MUSIC, 0);
+        }
+
         // Initialize soundboard images
         initializeSoundboardImages();
 
@@ -40,12 +62,21 @@ public class PicassoActivity extends AppCompatActivity {
 
         GridView gridView = (GridView) findViewById(R.id.heenok_grid_view);
         gridView.setAdapter(new ImageListAdapter(PicassoActivity.this, imgResourcesHeenok));
-    }
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "" + position, Toast.LENGTH_SHORT).show();
+            }
 
-    public void onSoundButtonClick(View view) {
-        Toast.makeText(getApplicationContext(), "Coming soon !", Toast.LENGTH_SHORT).show();
+        });
     }
-
+//
+//    public void playSound(View v) {
+//        if (loaded) {
+//            String soundFileName = v.getTag().toString();
+//            this.soundPool.play(soundResIds.get(soundFileName), 1, 1, 1, 0, 1);
+//        }
+//    }
 
     /* Helper methods */
     private void initializeSoundboardImages() {
